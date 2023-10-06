3e8e046cde4d4b6c5727fece4e7e3cfc925e8496
Hemyulin <93259659+Hemyulin@users.noreply.github.com>
2023-10-06 21:50:14 +0200

    ignore push if same offer in list

--- apartmentChecker.js
+++ apartmentChecker.js
@@ -1,56 +1,67 @@
 const puppeteer = require("puppeteer");
 const Pushover = require("pushover-notifications");
 
-async function main() {
-  while (true) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+// Declare the currentTime variable outside of the callback function
+let currentTime;
 
-    await page.goto("https://www.wbm.de/wohnungen-berlin/angebote/");
-    await new Promise((r) => setTimeout(r, 5000));
+// Maintain a set to store the previously found offer links
+const previousOfferLinks = new Set();
 
-    // Get the current date and time
-    const currentTime = new Date().toLocaleString();
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const currentContent = await page.content();
+  await page.goto("https://www.wbm.de/wohnungen-berlin/angebote/");
+  await new Promise((r) => setTimeout(r, 5000));
 
-    // Check if the specific message is present
-    if (
-      currentContent.includes(
-        "LEIDER HABEN WIR DERZEIT KEINE VERFÜGBAREN WOHNUNGSANGEBOTE"
-      )
-    ) {
-      console.log(`[${currentTime}] No new apartments found.`);
-    } else {
-      // Extract apartment offers
-      const offers = await page.evaluate(() => {
-        const offerElements = Array.from(
-          document.querySelectorAll(".row.openimmo-search-list-item")
-        );
-        return offerElements.map((element) => {
-          const title = element.querySelector(".imageTitle").textContent.trim();
-          const address = element.querySelector(".address").textContent.trim();
-          const link = element
-            .querySelector('a[title="Details"]')
-            .getAttribute("href");
-          return { title, address, link };
-        });
+  // Get the current date and time
+  currentTime = new Date().toLocaleString();
+
+  const currentContent = await page.content();
+
+  // Check if the specific message is present
+  if (
+    currentContent.includes(
+      "LEIDER HABEN WIR DERZEIT KEINE VERFÜGBAREN WOHNUNGSANGEBOTE"
+    )
+  ) {
+    console.log(`[${currentTime}] No new apartments found.`);
+  } else {
+    // Extract apartment offers
+    const offers = await page.evaluate(() => {
+      const offerElements = Array.from(
+        document.querySelectorAll(".row.openimmo-search-list-item")
+      );
+      return offerElements.map((element) => {
+        const title = element.querySelector(".imageTitle").textContent.trim();
+        const address = element.querySelector(".address").textContent.trim();
+        const link = element
+          .querySelector('a[title="Details"]')
+          .getAttribute("href");
+        return { title, address, link };
       });
+    });
 
-      // Send notifications for each offer
-      for (const offer of offers) {
+    // Send notifications for each new offer
+    for (const offer of offers) {
+      if (!previousOfferLinks.has(offer.link)) {
+        // Offer is not in the set of previously found offers
         const message = `${offer.title}\n${offer.address}\nLink: https://www.wbm.de${offer.link}`;
         console.log(`[${currentTime}] New apartment found: ${message}`);
         sendNotification(message, currentTime);
+        previousOfferLinks.add(offer.link); // Add the offer link to the set
       }
     }
-
-    await browser.close();
-
-    // Wait for 7 minutes (or any desired interval) before checking again
-    await new Promise((r) => setTimeout(r, 7 * 60 * 1000)); // 7 minutes
   }
-}
+
+  await browser.close();
+
+  // Wait for 7 minutes (or any desired interval) before checking again
+  await new Promise((r) => setTimeout(r, 7 * 60 * 1000)); // 7 minutes
+
+  // Restart the script to clear the set of previously found offers
+  process.exit();
+})();
 
 function sendNotification(message, currentTime) {
   const push = new Pushover({
@@ -68,7 +79,3 @@ function sendNotification(message, currentTime) {
     console.log(`[${currentTime}] Push notification sent successfully!`);
   });
 }
-
-main().catch((error) => {
-  console.error("An error occurred:", error);
-});
